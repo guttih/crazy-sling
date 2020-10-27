735b4d275cd725895b8387d532ede4312fbe91c2
guttih <gudjonholm@gmail.com>
2020-10-27 23:19:37 +0000

    adding project

---
+++ CrazySling/Assets/Bird.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+    
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+}
